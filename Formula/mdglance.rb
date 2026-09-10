69e6abc5fbb20aa54661580c43e22e5fd5363a6c
NepomukWolf <241941592+NepomukWolf@users.noreply.github.com>
2026-09-10 13:39:53 +0000

    mdglance: add 0.2.0_1 bottle.

--- Formula/mdglance.rb
+++ Formula/mdglance.rb
@@ -7,6 +7,11 @@ class Mdglance < Formula
   revision 1
   head "https://github.com/NepomukWolf/mdglance.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/nepomukwolf/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "f04abefa05efdf71ad6f177c35dba30dff83b8d62c357fa3ac742f0da828de3a"
+  end
+
   depends_on "rust" => :build
   depends_on :macos
 

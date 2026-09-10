6599c503e1a5a6758431b0b3934f83d14bd10a0f
Nepomuk Wolf <nepomuk.wolf@tum.de>
2026-09-10 15:47:31 +0200

    Build mdglance bottle for macOS 15
    Closes #4.
    
    Signed-off-by: NepomukWolf <241941592+NepomukWolf@users.noreply.github.com>

--- Formula/mdglance.rb
+++ Formula/mdglance.rb
@@ -4,13 +4,8 @@ class Mdglance < Formula
   url "https://static.crates.io/crates/mdglance/mdglance-0.2.0.crate"
   sha256 "d1f426721b6d97b266f5733b71dcd39dd2702196afe24fb15ffff9b06e66d301"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/NepomukWolf/mdglance.git", branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/nepomukwolf/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "f04abefa05efdf71ad6f177c35dba30dff83b8d62c357fa3ac742f0da828de3a"
-  end
 
   depends_on "rust" => :build
   depends_on :macos

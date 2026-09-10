fda1023ebbd976b39bfc4b52f71dc250a04a41c5
Nepomuk Wolf <nepomuk.wolf@tum.de>
2026-09-10 11:40:59 +0200

    Add mdglance 0.2.0

---
+++ Formula/mdglance.rb
@@ -0,0 +1,18 @@
+class Mdglance < Formula
+  desc "Keyboard-first Markdown and SVG previewer for terminal workflows"
+  homepage "https://github.com/NepomukWolf/mdglance"
+  url "https://static.crates.io/crates/mdglance/mdglance-0.2.0.crate"
+  sha256 "d1f426721b6d97b266f5733b71dcd39dd2702196afe24fb15ffff9b06e66d301"
+  license "MIT"
+  head "https://github.com/NepomukWolf/mdglance.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mdglance --version")
+  end
+end
